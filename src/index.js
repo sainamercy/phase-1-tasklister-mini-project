1dada896ab0050795aeabb2d780e90cafb5c4223
sainamercy <sainamercy@gmail.com>
2022-12-01 05:31:24 +0300

    delete task implemented

--- src/index.js
+++ src/index.js
@@ -5,12 +5,21 @@ document.addEventListener("DOMContentLoaded", () => {
   const renderTodoItem = function (e) {
     e.preventDefault();
     const todoItemInput = document.querySelector("#new-task-description").value;
-    console.log(todoItemInput);
     const todoList = document.querySelector("#tasks");
     const item = document.createElement("li");
     item.textContent = todoItemInput;
     // newest to do item to appear first in the list
     todoList.prepend(item);
+
+    // deleting task
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = "x";
+    item.append(deleteBtn);
+    const deleteTodo = function () {
+      const li = document.querySelector("li");
+      li.remove();
+    };
+    deleteBtn.addEventListener("click", deleteTodo);
   };
 
   form.addEventListener("submit", renderTodoItem);
