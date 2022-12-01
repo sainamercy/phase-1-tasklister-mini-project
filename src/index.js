4f32ed0792cccf8de048fcaea9984fd261570d87
sainamercy <sainamercy@gmail.com>
2022-12-01 05:04:36 +0300

    assignment completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("#create-task-form");
+
+  // function to render todo items
+  const renderTodoItem = function (e) {
+    e.preventDefault();
+    const todoItemInput = document.querySelector("#new-task-description").value;
+    console.log(todoItemInput);
+    const todoList = document.querySelector("#tasks");
+    const item = document.createElement("li");
+    item.textContent = todoItemInput;
+    // newest to do item to appear first in the list
+    todoList.prepend(item);
+  };
+
+  form.addEventListener("submit", renderTodoItem);
 });
